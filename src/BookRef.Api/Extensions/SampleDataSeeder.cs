1d987f1e5da7264beaddb8cbbe7ce3da8da81c3f
Fabian Juette <Fabian.Juette@tu-clausthal.de>
2020-12-11 18:38:51 +0100

    add more book examples

--- src/BookRef.Api/Extensions/SampleDataSeeder.cs
+++ src/BookRef.Api/Extensions/SampleDataSeeder.cs
@@ -17,12 +17,18 @@ namespace BookRef.Api.Extensions
 
         public void SeedAll()
         {
-            var danAuthor = new Author("Dan O'Riley");
+            var danAuthor = new Author("Dan Ariely");
             _context.Attach(danAuthor);
             var hansAuthor = new Author("Hans Rosling");
             _context.Attach(hansAuthor);
             var juliaAuthor = new Author("Julia Shaw");
             _context.Attach(juliaAuthor);
+            var zimbardoAuthor = new Author("Philip Zimbardo");
+            _context.Attach(zimbardoAuthor);
+            var freudAuthor = new Author("Sigmund Freud");
+            _context.Attach(freudAuthor);
+            var bernaysAuthor = new Author("Edward Bernays");
+            _context.Attach(bernaysAuthor);
 
             var categoryGehirn = new Category("Gehirn");
             _context.Attach(categoryGehirn);
@@ -37,6 +43,7 @@ namespace BookRef.Api.Extensions
             var speakerRike = new Speaker("Rike Schmid");
             _context.Attach(speakerRike);
 
+
             var user = new User
             {
                 EMail = "fabian.j@test.de",
@@ -47,12 +54,12 @@ namespace BookRef.Api.Extensions
 
             var book = new Book
             {
-                ISBN = "12321321321",
-                Title = "Denken hilft zwar, nützt aber nichts",
+                ISBN = "978-3426300886",
+                Title = "Denken hilft zwar, nützt aber nichts: Warum wir immer wieder unvernünftige Entscheidungen",
                 Language = Language.German,
-                Link = "https://google.de",
-                Created = System.DateTime.Now,
-                Auflage = "Erste",
+                Link = "https://www.amazon.de/Denken-hilft-zwar-n%C3%BCtzt-nichts/dp/3426300885",
+                Created = new System.DateTime(2020, 05, 11),
+                Auflage = "Sechste",
                 Creator = user
             };
             book.SetAuthors(new List<Author> { danAuthor });
@@ -62,12 +69,12 @@ namespace BookRef.Api.Extensions
 
             var book2 = new Book
             {
-                ISBN = "345890438590345",
-                Title = "Das Trügerische Gehirn",
+                ISBN = "978-3453604483",
+                Title = "Das trügerische Gedächtnis: Wie unser Gehirn Erinnerungen fälscht",
                 Language = Language.German,
-                Link = "https://google.de",
-                Created = System.DateTime.Now,
-                Auflage = "Dritte",
+                Link = "https://www.amazon.de/Das-tr%C3%BCgerische-Ged%C3%A4chtnis-Erinnerungen-f%C3%A4lscht/dp/3453604482",
+                Created = new System.DateTime(2020, 06, 12),
+                Auflage = "Erste",
                 Creator = user
             };
             book2.SetAuthors(new List<Author> { juliaAuthor, hansAuthor });
@@ -75,6 +82,66 @@ namespace BookRef.Api.Extensions
             _context.Add(book2);
             user.AddNewBook(book2);
             user.AddBookRecommendation(book, book2, "Sie findet das Buch ganz toll");
+
+            var book3 = new Book
+            {
+                ISBN = "978-3446260290",
+                Title = "Böse: Die Psychologie unserer Abgründe",
+                Language = Language.German,
+                Link = "https://www.amazon.de/B%C3%B6se-Die-Psychologie-unserer-Abgr%C3%BCnde/dp/3446260293",
+                Created = new System.DateTime(2020, 07, 15),
+                Auflage = "Dritte",
+                Creator = user
+            };
+            book3.SetAuthors(new List<Author> { juliaAuthor });
+            book3.SetCategories(new List<Category> { categoryGehirn });
+            _context.Add(book3);
+            user.AddNewBook(book3);
+
+            var book4 = new Book
+            {
+                ISBN = "978-3662533253",
+                Title = "Der Luzifer-Effekt: Die Macht der Umstände und die Psychologie des Bösen",
+                Language = Language.German,
+                Link = "https://www.amazon.de/B%C3%B6se-Die-Psychologie-unserer-Abgr%C3%BCnde/dp/3446260293",
+                Created = new System.DateTime(2020, 07, 18),
+                Auflage = "Erste",
+                Creator = user
+            };
+            book4.SetAuthors(new List<Author> { zimbardoAuthor });
+            book4.SetCategories(new List<Category> { categoryGehirn });
+            _context.Add(book4);
+            user.AddNewBook(book4);
+
+            var book5 = new Book
+            {
+                ISBN = "978-3730604540",
+                Title = "Massenpsychologie und Ich-Analyse",
+                Language = Language.German,
+                Link = "https://www.amazon.de/Massenpsychologie-Ich-Analyse-Sigmund-Freud/dp/3730604546",
+                Created = new System.DateTime(2020, 08, 20),
+                Auflage = "Erste",
+                Creator = user
+            };
+            book5.SetAuthors(new List<Author> { freudAuthor });
+            book5.SetCategories(new List<Category> { categoryGehirn });
+            _context.Add(book5);
+            user.AddNewBook(book5);
+
+            var book6 = new Book
+            {
+                ISBN = "978-3936086355",
+                Title = "Propaganda: Die Kunst der Public Relations",
+                Language = Language.German,
+                Link = "https://www.amazon.de/Propaganda-Die-Kunst-Public-Relations/dp/3936086354",
+                Created = new System.DateTime(2020, 09, 1),
+                Auflage = "Dritte",
+                Creator = user
+            };
+            book6.SetAuthors(new List<Author> { bernaysAuthor });
+            book6.SetCategories(new List<Category> { categoryGehirn });
+            _context.Add(book6);
+            user.AddNewBook(book6);
 
             _context.SaveChanges();
             TestSeededData();
